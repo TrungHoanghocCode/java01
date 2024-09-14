f90e84688ccf73d1cc32610179b6b2be06b41871
TrungHoangHocCode <trungdau97@gmail.com>
2024-09-14 14:22:36 +0700

    init project

---
+++ src/firstProject/HelloWorld.java
@@ -0,0 +1,12 @@
+package firstProject;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World with trungHocCode");
+		System.out.println("Hello World with trungHocCode");
+		System.out.println("Hello World with trungHocCode");
+		System.out.println("Hello World with trungHocCode");
+
+	}
+
+}
